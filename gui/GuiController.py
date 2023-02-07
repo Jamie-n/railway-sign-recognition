224da998a7e84c9ca7fe7502cc059fd86d8d41a2
Jamie Neighbours <jamieneighbours@gmail.com>
2023-02-07 17:01:53 +0000

    Add methods to start and stop detection

--- gui/GuiController.py
+++ gui/GuiController.py
@@ -1,9 +1,27 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.core.window import Window
+from kivy.uix.popup import Popup
+
+
+class SettingsPopup(Popup):
+    def save_settings(self):
+        self.dismiss()
+
 
 class MyGridLayout(Widget):
-    pass
+
+    def open_settings(self):
+        SettingsPopup().open()
+
+    def begin_detection(self):
+        self.ids.stop_detection_button.disabled = False
+        self.ids.start_detection_button.disabled = True
+
+    def halt_detection(self):
+        self.ids.start_detection_button.disabled = False
+        self.ids.stop_detection_button.disabled = True
+        self.ids.current_limit_value.text = "00"
 
 
 class SpeedControllerApp(App):
