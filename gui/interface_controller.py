f2c62796f27f7ed9b523bd59776d1bc86a5f8006
Jamie Neighbours <56030151+Jamie-n@users.noreply.github.com>
2023-02-26 19:30:57 +0000

    Refactor interface into KivyMD (#7)
    * Refactor user interface into KivyMD

--- gui/interface_controller.py
+++ gui/interface_controller.py
@@ -1,12 +1,13 @@
-from kivy.app import App
+from kivymd.app import MDApp
+from kivy.base import Builder
 from kivy.uix.widget import Widget
+from gui.settings_controller import SettingsMenuContent
 from kivy.core.window import Window
-from gui.settings_controller import SettingsPopup
 
 class InterfaceController(Widget):
 
     def open_settings(self):
-        SettingsPopup().show_settings()
+        SettingsMenuContent().show_settings()
 
     def begin_detection(self):
         self.ids.stop_detection_button.disabled = False
@@ -17,9 +18,15 @@ class InterfaceController(Widget):
         self.ids.stop_detection_button.disabled = True
         self.ids.current_limit_value.text = "00"
 
-class SpeedControllerApp(App):
+
+class SpeedControllerApp(MDApp):
     def build(self):
         Window.size = (1260, 720)
+
+        Builder.load_file('gui/layouts/main_interface.kv')
+        Builder.load_file('gui/layouts/settings_menu.kv')
+        Builder.load_file('gui/layouts/capture_preview.kv')
+
 
         return InterfaceController()
 
